2edcceaad8dcf2b5197c9ff24edac4206997e0cf
annarischitelli <annarischitelli94@gmail.com>
2021-01-27 16:56:52 -0700

    got first steps working but not showing

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,11 +6,38 @@
 // You'll code this graphic in the app.js file of your homework directory—make sure you pull in the data from data.csv by using the d3.csv function. 
 // Your scatter plot should ultimately appear like the image at the top of this section.
 
-// Create chart area for the plot to be added to
-// Set dimensions of chart area
+// Create chart area and set dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 // Pull in D3 File from CVS
-// Make sure data is working
+d3.csv("./data.csv").then(function(Data) {
+    // Make sure data is working
+    console.log(Data);
+    
+
 // Create function that will pull required chart data
 // Format chart
 // Create circles
 // Label axis
+
+});
